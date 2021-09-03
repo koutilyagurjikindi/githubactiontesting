15b7ac12c9a74670b8b8432e0f9972ecf6347dae
koutilya goud <personalmac@koutilyas-MacBook-Pro.local>
2021-09-03 14:05:33 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var http = require('http')
+var server = http.createServer(function(req,res){
+//handle incoming requests here	
+});
+server.listen(5000);
+console.log('Node.js web server at port 5000 is running')
